bdcd5e065465e5fa15cbf8b349dcea09ed74c6ad
nathanq <nathanstefanqian@gmail.com>
2024-07-12 12:19:32 +0800

    init()初始化pinia，准备修复auto-import

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { createSSRApp } from "vue";
 import App from "./App.vue";
+import pinia from './pinia'; // 引入配置好的Pinia
 import 'virtual:windi.css'
 export function createApp() {
   const app = createSSRApp(App);
+  app.use(pinia)
   return {
-    app,
+    app
   }
 }
--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,15 +1,21 @@
 <template>
-<div class="bg-[blue]">
-  <button @click="sendSms">发送</button>
-</div>
+  <div>
+    <p>Count: {{ counterStore.count }}</p>
+    <p>Double Count: {{ doubleCount }}</p>
+    <button @click="increment">Increment</button>
+    <button @click="decrement">Decrement</button>
+    <button @click="reset">Reset</button>
+  </div>
 </template>
 
 <script setup lang="ts">
-import { sendSms } from '@/api/home'
+import { useCounterStore } from '@/pinia/counter';
 import { ref } from 'vue'
-const title = ref('Hello world!')
+const counterStore = useCounterStore();
+
+const count = ref(counterStore.count);
+const doubleCount = counterStore.doubleCount;
+const increment = counterStore.increment;
+const decrement = counterStore.decrement;
+const reset = counterStore.reset;
 </script>
-
-<style>
-
-</style>
